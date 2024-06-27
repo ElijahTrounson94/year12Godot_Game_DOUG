ce06c0a6b31e2fe83f1a0b6363847c6324b5b31a
ElijahTrounson94 <elitro94@kingshigh.school.nz>
2024-06-27 12:23:28 +1200

    added pistol crosshair to display, and started making another to add for another gun

---
+++ Modelling/controller.gd
@@ -0,0 +1,61 @@
+extends CharacterBody3D
+
+@export var speed = 5
+@export var sprint = 8
+@export var mouse_sensitivity = 0.003
+@onready var camera = $Camera3D
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var jump_speed = 5
+
+@onready var crosshairImage = $"Pistol crosshair/Pistol crosshair"
+
+@onready var ray = $Camera3D/RayCast3D
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	var input_dir = Input.get_vector("left","right","up","down")
+	var move_dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y).normalized()
+	if input_dir:
+		velocity.x = move_dir.x * speed
+		velocity.z = move_dir.z * speed
+	else:
+		velocity.x = 0
+		velocity.z = 0
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y = jump_speed
+	move_and_slide()
+	
+	if ray.is_colliding():
+		check_collisions()
+	
+
+	
+	if Input.is_action_pressed("Sprint"):
+		speed = 8
+	else:
+		speed = 5 
+	
+
+func check_collisions():
+	var collider = ray.get_collider()
+	
+	
+	
+	
+	
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+		crosshairImage.texture = load("res://crosshair.png");
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+		crosshairImage.texture = load("res://20197.png");
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x,-1,1)
+		
+		
