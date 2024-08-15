8172288e5fdf4f85729b0f1b0a9ea1082563819f
ElijahTrounson94 <elitro94@kingshigh.school.nz>
2024-08-15 12:25:20 +1200

    editing Imp pathfinding

--- Modelling/imp.gd
+++ Modelling/imp.gd
@@ -3,19 +3,32 @@ extends CharacterBody3D
 var speed = 3
 var accel = 8
 
-@onready var nav: NavigationAgent3D = $NavigationAgent3D
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var player : CharacterBody3D = get_tree().get_first_node_in_group("player");
 
 func _physics_process(delta):
-	nav.target_position = player.global_position;
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_location()
+	var new_velocity = (next_location - current_location).normalised() * speed
 	
-	var direction = (player.global_position - nav.get_next_path_position()).normalized();
-	
-	if not is_on_floor():
-		direction.y -= 9.8*delta;
-	else:
-		direction.y -= 2;
-	
-	velocity = velocity.move_toward(direction * speed , accel * delta);
-	
-	move_and_slide();
+	velocity = new_velocity
+	move_and_slide()
+
+#
+	#nav_agent.target_position = player.global_transform.origin
+	#
+	#var direction = nav_agent.get_next_path_position()
+	#
+	#"""
+	#if not is_on_floor():
+		#direction.y -= 9.8*delta;
+	#else:
+		#direction.y -= 2;
+	#"""
+	#
+#
+	#
+	#
+	#velocity = velocity.move_toward(direction * speed , accel * delta);
+	#
+	#move_and_slide();
